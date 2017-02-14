d5b94bfced43ba9d572bcc851e6f8216d595eea5
Woh Shon <wohshon@gmail.com>
2017-02-14 18:54:20 +0800

    initial

--- application.js
+++ application.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 
 // list the endpoints which you want to make securable here
 var securableEndpoints;
-securableEndpoints = ['/hello'];
+securableEndpoints = ['/hello', '/product'];
 
 var app = express();
 
@@ -23,11 +23,11 @@ app.use(express.static(__dirname + '/public'));
 app.use(mbaasExpress.fhmiddleware());
 
 app.use('/hello', require('./lib/hello.js')());
-
+app.use('/product', require('./lib/product.js')());
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
 
-var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8011;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, host, function() {
   console.log("App started at: " + new Date() + " on port: " + port); 
